32159eec1d853b0e1249766b11643c8cb9a20941
Charly Correa M. <chuck.scrumdev@gmail.com>
2025-01-07 12:58:34 -0500

    delete currency module completly

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideEnvironmentNgxCurrency, NgxCurrencyInputMode } from 'ngx-currency';
 import { provideHttpClient } from '@angular/common/http';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
