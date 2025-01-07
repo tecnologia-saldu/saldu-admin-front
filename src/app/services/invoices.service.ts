699750e0d2a10f4be2981d4ac0883548fe3fa97a
Juliana Cardona <julicardonco@hotmail.com>
2025-01-07 13:28:32 -0500

    Deleted headers from invoice service

--- src/app/services/invoices.service.ts
+++ src/app/services/invoices.service.ts
@@ -12,39 +12,23 @@ interface StripeResponse {
 export class InvoicesService {
   private http = inject(HttpClient);
   private apiUrl = 'http://ec2-3-144-209-131.us-east-2.compute.amazonaws.com:3001/invoice';
-  private apiKey = ''
-
 
 
   constructor() { }
 
   getInvoices() {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.apiKey}`
-    });
-
-    return this.http.post<Invoice[]>(`${this.apiUrl}/all-pending`, { headers });
+    return this.http.post<Invoice[]>(`${this.apiUrl}/all-pending`, '');
   }
 
   getOneInvoice(id: string) {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.apiKey}`
-    });
-
-    return this.http.get<Invoice>(`${this.apiUrl}/${id}`, { headers });
+    return this.http.get<Invoice>(`${this.apiUrl}/${id}`);
   }
 
   uploadToSiigo(id: number) {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.apiKey}`
-    });
-    return this.http.post<Invoice>(`${this.apiUrl}/siigo/${id}`, { headers })
+    return this.http.post<Invoice>(`${this.apiUrl}/siigo/${id}`, '')
   }
 
   updateInvoiceInfo(id: number, commission: number) {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.apiKey}`
-    });
-    return this.http.put<Invoice>(`${this.apiUrl}/${id}`, { commission }, { headers })
+    return this.http.put<Invoice>(`${this.apiUrl}/${id}`, { commission })
   }
 }
